4006946509dea5fd0e5164ef9e6e73c540c88ffc
Vu Nguyen <vunh811@gmail.com>
2021-04-29 18:38:28 +0700

    set up sequelize

---
+++ server/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const PORT = process.env.PORT || 5000
+const app = express()
+const models = require('./models')
+
+dotenv.config()
+
+app.use(express.json())
+
+app.get('/api/test', (req, res) => {
+    res.json({'message': 'ok'})
+})
+
+models.sequelize.sync().then(() => {
+    console.log('Db Nice!')
+}).catch((err) => {
+    console.log(err, 'Error Database!')
+})
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    console.error(err.message, err.stack)
+    res.status(statusCode).json({'message': err.message})
+    return
+})
+
+app.listen(PORT, console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
---
+++ server/src/models/book.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Book.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
