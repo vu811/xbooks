6c7a0dacb362cb804baa3f09acccad6b72804000
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2021-05-24 20:29:30 +0700

    Install Ant Design & setup layout

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import styles from './Footer.module.scss';
+
+const Footer: FC = () => {
+  return (
+    <div className={styles.footer}>X-Books ©2021 Created by Vu + Tuong</div>
+  );
+};
+
+export default Footer;
