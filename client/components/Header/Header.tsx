5f97a2cbb29ccc13353b03010f33fd12450ffb61
Vu Nguyen <vunh811@gmail.com>
2021-05-26 18:46:41 +0700

    theme setup

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,63 +1,26 @@
+import { Input } from 'antd';
+import React, { FC } from 'react';
 import {
-  GiftOutlined,
-  LoginOutlined,
-  ReadOutlined,
-  UserAddOutlined,
+  UserOutlined 
 } from '@ant-design/icons';
-import { Menu } from 'antd';
-import Link from 'next/link';
-import React, { FC, useState } from 'react';
+
 import styles from './Header.module.scss';
-import { useRouter } from 'next/router';
 
-const { SubMenu } = Menu;
-
-const Header: FC = () => {
-  const router = useRouter();
-  const [current, setCurrent] = useState('mail');
-  const handleClick = (e) => {
-    console.log('click ', e);
-    setCurrent(e.key);
-    router.push(`/${e.key}`);
-  };
-
+const Header : FC = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        {/* <Link href='/'>
-          <img src='/logo-x.png' alt='logo' onClick={() => setCurrent(null)} />
-        </Link> */}
-        XBooks
-      </div>
-      <div className={styles.menu}>
-        <Menu
-          onClick={handleClick}
-          selectedKeys={[current]}
-          mode='horizontal'
-          theme='dark'
-        >
-          <SubMenu key='books' icon={<ReadOutlined />} title='Books'>
-            <Menu.ItemGroup title='Nonfiction'>
-              <Menu.Item key='business'>Business/economics</Menu.Item>
-              <Menu.Item key='health'>Health/fitness</Menu.Item>
-              <Menu.Item key='science'>Science</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title='Fiction'>
-              <Menu.Item key='romance'>Romance</Menu.Item>
-              <Menu.Item key='horror'>Horror</Menu.Item>
-              <Menu.Item key='comic'>Comic book</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-          <Menu.Item key='giveaway' disabled icon={<GiftOutlined />}>
-            Giveaway
-        </Menu.Item>
-          <Menu.Item key='register' icon={<UserAddOutlined />}>
-            Register
-        </Menu.Item>
-          <Menu.Item key='login' icon={<LoginOutlined />}>
-            Login
-        </Menu.Item>
-        </Menu>
+    <header className={styles.header} >
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          XBOOKS
+        </div>
+        <div className={styles.headerLineSecond}>
+          <div className={styles.searchBox}>
+          <Input placeholder='Tìm kiếm trên XBOOKS' />
+          </div>
+          <div className={styles.login}><UserOutlined />
+            <span className={styles.loginText}>Đăng nhập</span>
+          </div>
+        </div>
       </div>
     </header>
   );
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -7,7 +7,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>X Books - Buy, sell and giveaway books.</title>
+      <title>XBOOKS - Buy, sell and giveaway books.</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='icon' href='/favicon.png' />
       </Head>
       <Layout>
